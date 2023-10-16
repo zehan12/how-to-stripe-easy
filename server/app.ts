5bd49263453c1e490b59653038930af29c0ee4bd
Zehan Khan <zkhan@hootboard.com>
2023-10-17 00:08:36 +0530

    feat: :sparkles: API is created for getting client secret and to further payment

--- server/app.ts
+++ server/app.ts
@@ -1,15 +1,52 @@
-import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
-
-dotenv.config();
+import env from 'dotenv';
+import express, { Express, Request, Response } from 'express';
+import cors from "cors";
+import Stripe from "stripe";
 
 const app: Express = express();
-const port = process.env.PORT;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+    apiVersion: '2023-08-16',
+  });
+  
+
+app.post("/create-payment-intent", async (_:Request, res:Response) => {
+  console.log("req");
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: 1,
+      currency: "usd",
+      // desc & shipping is require if you are using this service in india
+      description: "Software development services",
+      shipping: {
+        name: "Zehan Khan",
+        address: {
+          line1: "510 Townsend St",
+          postal_code: "98140",
+          city: "San Francisco",
+          state: "CA",
+          country: "US",
+        },
+      },
+      receipt_email: "zehan9211@gmail.com",
+      payment_method_types: ["card"],
+      metadata: { integration_check: "accept_a_payment" },
+    });
+    res.status(200).json({
+      message: "Send client secret",
+      clientSecret: paymentIntent.client_secret,
+    });
+  } catch (error:any) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+
+app.listen(3000, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${3000}`);
 });
